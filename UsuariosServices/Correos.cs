c3d5425236a99abd1bc0b53caad489dcda4c6ab9
jurguenzambrano <jurguenzambrano@gmail.com>
2016-09-23 15:52:48 -0500

    Send mail

--- UsuariosServices/Correos.cs
+++ UsuariosServices/Correos.cs
@@ -13,7 +13,7 @@ namespace UsuariosServices
          * Gmail:  smtp.gmail.com  puerto:587
          * Hotmail: smtp.liva.com  puerto:25
          */
-        SmtpClient server = new SmtpClient("smtp.mailgun.org", 587);
+        SmtpClient server = new SmtpClient("mailtrap.io", 2525);
 
         public Correos()
         {
@@ -24,7 +24,7 @@ namespace UsuariosServices
              * Direccion de Correo (Gmail o Hotmail)
              * y Contrasena correspondiente
              */
-            server.Credentials = new System.Net.NetworkCredential("postmaster@sandboxd624b64941b5466abb1e5b07f516f69c.mailgun.org", "f931c7b5a2de9e2565b86ab07d869f26");
+            server.Credentials = new System.Net.NetworkCredential("315e8416980e6f", "6241a427f47452");
             server.EnableSsl = true;
         }
 
